05f7458ee2adc2902485a6230486ca8a86487b69
Ali Hamza <150916400+AliHamza02088@users.noreply.github.com>
2024-09-23 19:54:40 +0500

    Delete login directory

--- login/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from home import views
-from django.urls import path,include
-
-urlpatterns = [
-    path('index/', views.index, name = "home"),
-    path('', views.loginUser, name = "login"),
-    path('logout/', views.logoutUser, name = "logout"),
-]
